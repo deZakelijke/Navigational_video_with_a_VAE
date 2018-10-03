5ce52b493292f163f826ae28b2b0e1346bd3e7de
Micha de Groot <git@michadegroot.nl>
2018-10-03 11:58:14 +0200

    Made mu and logvar encoding two separate networks

--- src/VAE.py
+++ src/VAE.py
@@ -19,12 +19,18 @@ class VAE(nn.Module):
         self.flat_dims = 512
 
         # Define encoding layers
-        # Should I change the kernel and stride because I scaled to 128x128 instead of 64x64?
-        self.conv1      = nn.Conv2d(self.chns, self.chns, (4, 4))
-        self.conv2      = nn.Conv2d(self.chns, self.filters, (4, 4), stride=(4, 4))
-        self.conv3      = nn.Conv2d(self.filters, self.filters, (6, 6), stride=(2, 2))
-        self.conv4      = nn.Conv2d(self.filters, self.filters, (6, 6), stride=(2, 2))
+        # Should I change the kernel and stride because I scaled to 128x128 instead of 64x64? Did so
+        # Will revert the descision to infer the mu and sigmoid with the same conv layers.
+        self.conv1_mu   = nn.Conv2d(self.chns, self.chns, (4, 4))
+        self.conv2_mu   = nn.Conv2d(self.chns, self.filters, (4, 4), stride=(4, 4))
+        self.conv3_mu   = nn.Conv2d(self.filters, self.filters, (6, 6), stride=(2, 2))
+        self.conv4_mu   = nn.Conv2d(self.filters, self.filters, (6, 6), stride=(2, 2))
         self.fc_mu      = nn.Linear(self.flat_dims, self.latent_dims)
+
+        self.conv1_var  = nn.Conv2d(self.chns, self.chns, (4, 4))
+        self.conv2_var  = nn.Conv2d(self.chns, self.filters, (4, 4), stride=(4, 4))
+        self.conv3_var  = nn.Conv2d(self.filters, self.filters, (6, 6), stride=(2, 2))
+        self.conv4_var  = nn.Conv2d(self.filters, self.filters, (6, 6), stride=(2, 2))
         self.fc_logvar  = nn.Linear(self.flat_dims, self.latent_dims)
 
         # Define decoding layers
@@ -41,12 +47,19 @@ class VAE(nn.Module):
 
 
     def encode(self, x):
-        h1  = self.relu(self.conv1(x))
-        h2  = self.relu(self.conv2(h1))
-        h3  = self.relu(self.conv3(h2))
-        h4  = self.relu(self.conv4(h3))
-        h5  = h4.view(-1, self.flat_dims)
-        return self.sigmoid(self.fc_mu(h5)), self.sigmoid(self.fc_logvar(h5))
+        h1_mu  = self.relu(self.conv1_mu(x))
+        h2_mu  = self.relu(self.conv2_mu(h1_mu))
+        h3_mu  = self.relu(self.conv3_mu(h2_mu))
+        h4_mu  = self.relu(self.conv4_mu(h3_mu))
+        h5_mu  = h4_mu.view(-1, self.flat_dims)
+
+        h1_var = self.relu(self.conv1_var(x))
+        h2_var = self.relu(self.conv2_var(h1_var))
+        h3_var = self.relu(self.conv3_var(h2_var))
+        h4_var = self.relu(self.conv4_var(h3_var))
+        h5_var = h4_var.view(-1, self.flat_dims)
+
+        return self.sigmoid(self.fc_mu(h5_mu)), self.sigmoid(self.fc_logvar(h5_var))
 
     def reparametrise(self, mu, logvar):
         if self.training:
